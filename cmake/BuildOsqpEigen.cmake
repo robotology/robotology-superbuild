03afdcad184e67cbcf63d843bc7fab2bcf1d4abd
Stefano <stefano.dafarra@gmail.com>
2018-06-13 13:39:46 +0200

    Added files to add osqp, OsqpEigen, UnicyclePlanner and walking-controllers to the superbuild.
    Modified .gitignore to ignore .user files (QTCreator).

---
+++ cmake/BuildOsqpEigen.cmake
@@ -0,0 +1,16 @@
+# Copyright (C) 2018  iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Stefano Dafarra <stefano.dafarra@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(osqp QUIET)
+
+ycm_ep_helper(OsqpEigen TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology/osqp-eigen.git
+              TAG master
+              COMPONENT dynamics
+              FOLDER robotology
+              CMAKE_ARGS -DBUILD_TESTING:BOOL=OFF
+              DEPENDS osqp)
---
+++ cmake/Buildosqp.cmake
@@ -0,0 +1,13 @@
+# Copyright (C) 2018  iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Stefano Dafarra <stefano.dafarra@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+
+ycm_ep_helper(osqp TYPE GIT
+              STYLE GITHUB
+              REPOSITORY oxfordcontrol/osqp.git
+              TAG master
+              COMPONENT dynamics
+              FOLDER external
+              CMAKE_ARGS -DUNITTESTS:BOOL=OFF)
