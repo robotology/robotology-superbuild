c804822e078d7dc2a432d5d5816eb625131b3cb8
Silvio Traversaro <silvio.traversaro@iit.it>
2018-08-20 23:14:09 +0200

    Migrate Travis to use docker and test on Xenial and Bionic
    Fix https://github.com/robotology/robotology-superbuild/issues/96

---
+++ .ci/install_debian.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+sudo apt-get update
+sudo apt-get install build-essential cmake coinor-libipopt-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
