b6ad61ec5429e10bd07b7a3de5cb2d284c79cdcf
Silvio Traversaro <silvio.traversaro@iit.it>
2018-08-20 23:32:13 +0200

    Add clang and debian

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -2,4 +2,4 @@
 set -e
 
 apt-get update
-apt-get install -y build-essential cmake coinor-libipopt-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
+apt-get install -y build-essential cmake clang coinor-libipopt-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
