c4538f25b5b1a723c9c7789233c1c6ea99a23f46
Diego Ferigo <diego.ferigo@iit.it>
2019-01-18 12:51:13 +0100

    Added the BlockFactory project to the DYNAMICS profile

---
+++ cmake/BuildBlockFactory.cmake
@@ -0,0 +1,13 @@
+# Copyright (C) 2019 iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Diego Ferigo <diego.ferigo@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+
+ycm_ep_helper(BlockFactory TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology/blockfactory.git
+              TAG master
+              COMPONENT dynamics
+              FOLDER robotology
+              CMAKE_ARGS -DUSES_MATLAB:BOOL=${ROBOTOLOGY_USES_MATLAB})
