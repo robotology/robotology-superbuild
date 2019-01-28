1d66b3c0b594117c5ecb7393c99524072f69a635
Silvio Traversaro <pegua1@gmail.com>
2019-01-28 18:51:22 +0100

    Update cmake/Buildretargeting.cmake
    Co-Authored-By: kouroshD <kourosh.darvish@gmail.com>

--- cmake/Buildretargeting.cmake
+++ cmake/Buildretargeting.cmake
@@ -14,7 +14,7 @@ ycm_ep_helper(retargeting TYPE GIT
               TAG master
               COMPONENT teleoperation
               FOLDER robotology
-#              CMAKE_ARGS -DWALKING_TELEOPERATION_USES_CYBSDK:BOOL=${ROBOTOLOGY_USES_CYB_SDK}
+#              CMAKE_ARGS -DWALKING_TELEOPERATION_USES_CYBSDK:BOOL=${ROBOTOLOGY_USES_CYBERITH_SDK}
 #                         -DWALKING_TELEOPERATION_USES_OCULUSSDK:BOOL=${ROBOTOLOGY_USES_OCULUS_SDK}
               DEPENDS iDynTree
                       ICUB
