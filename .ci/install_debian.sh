b7ba61f4578e4ae84ecb413a4d1f20a2f1f759e0
Diego Ferigo <diego.ferigo@iit.it>
2019-04-03 16:34:45 +0200

    Enabled ccache in linux travis jobs

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -7,7 +7,7 @@ apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 # CI specific packages
-apt-get install -y clang valgrind
+apt-get install -y clang valgrind ccache
 
 # Core dependencies
 apt-get install -y build-essential cmake clang coinor-libipopt-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev libxml2-dev
