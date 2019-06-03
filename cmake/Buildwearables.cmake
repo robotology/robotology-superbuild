41c7a5b6f050867017277d7383be94cbbb93fadb
Kourosh Darvish <kourosh.darvish@gmail.com>
2019-06-03 16:25:35 +0200

    Add HDE option to super-build

---
+++ cmake/Buildwearables.cmake
@@ -0,0 +1,28 @@
+# Copyright (C) 2018  iCub Facility, Istituto Italiano di Tecnologia
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
+
+set(WEARABLES_DEPENDS "")
+list(APPEND WEARABLES_DEPENDS YARP)
+
+if(WIN32)
+    find_or_build_package(ICUB QUIET)
+    find_or_build_package(icub-firmware-shared QUIET)
+    find_or_build_package(forcetorque-yarp-devices QUIET)
+
+    list(APPEND WEARABLES_DEPENDS ICUB)
+    list(APPEND WEARABLES_DEPENDS icub-firmware-shared)
+    list(APPEND WEARABLES_DEPENDS forcetorque-yarp-devices)
+endif()
+
+ycm_ep_helper(wearables TYPE GIT
+              STYLE GITHUB
+              REPOSITORY robotology-playground/wearables.git
+              TAG master
+              COMPONENT human_dynamics
+              FOLDER robotology
+              DEPENDS ${WEARABLES_DEPENDS})
