14fa9d96f5f328b22ccc9be7fd60053b86ed01ce
Silvio Traversaro <silvio.traversaro@iit.it>
2019-10-25 10:45:40 +0200

    Update install_debian.sh

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -7,7 +7,7 @@ apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 # CI specific packages
-apt-get install -y clang valgrind ccache
+apt-get install -y clang valgrind ccache ninja-build
 
 # Core dependencies
 apt-get install -y libeigen3-dev build-essential cmake cmake-curses-gui coinor-libipopt-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libtinyxml-dev libace-dev libedit-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev qml-module-qtquick2 qml-module-qtquick-window2 qml-module-qtmultimedia qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings libsdl1.2-dev libxml2-dev
