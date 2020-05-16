4215086eb3689b6a52beb215092debe76e254e36
Silvio Traversaro <silvio.traversaro@iit.it>
2020-05-16 11:34:16 +0200

    Add missing dependencies of iDynTree on OsqpEigen  (#408)
    * Add missing dependencies of iDynTree on OsqpEigen
    
    Before this fix, whetever iDynTree was compiled or not with OsqpEigen support was non 
    deterministic, and based on wheter OsqpEigen  was already compiled and  installed when
    iDynTree was configured. With  this fix, the dependency of iDynTree on OsqpEigen is explicitly 
    indicated, and the IDYNTREE_USES_* options are explicity set, so that if for some reason one
    of the dependency is not installed correctly, iDynTree configuration will fail.

--- cmake/BuildiDynTree.cmake
+++ cmake/BuildiDynTree.cmake
@@ -7,10 +7,12 @@ include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
 find_or_build_package(ICUB QUIET)
+find_or_build_package(OsqpEigen QUIET)
 
 set(iDynTree_DEPENDS "")
 list(APPEND iDynTree_DEPENDS YARP)
 list(APPEND iDynTree_DEPENDS ICUB)
+list(APPEND iDynTree_DEPENDS OsqpEigen)
 
 ycm_ep_helper(iDynTree TYPE GIT
               STYLE GITHUB
@@ -19,7 +21,11 @@ ycm_ep_helper(iDynTree TYPE GIT
               COMPONENT dynamics
               FOLDER robotology
               CMAKE_ARGS -DIDYNTREE_USES_IPOPT:BOOL=ON
+                         -DIDYNTREE_USES_YARP:BOOL=ON
+                         -DIDYNTREE_USES_ICUB_MAIN:BOOL=ON
+                         -DIDYNTREE_USES_OSQPEIGEN:BOOL=ON
                          -DIDYNTREE_USES_MATLAB:BOOL=${ROBOTOLOGY_USES_MATLAB}
                          -DIDYNTREE_USES_PYTHON:BOOL=${ROBOTOLOGY_USES_PYTHON}
                          -DIDYNTREE_USES_OCTAVE:BOOL=${ROBOTOLOGY_USES_OCTAVE}
+                         
               DEPENDS ${iDynTree_DEPENDS})
