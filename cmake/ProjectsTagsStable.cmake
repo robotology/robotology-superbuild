21404c151a070848c85824324888e1bc24a63780
Silvio Traversaro <silvio.traversaro@iit.it>
2020-09-11 16:39:14 +0200

    Update ProjectsTagsStable.cmake

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -10,5 +10,4 @@ set_tag(osqp_TAG v0.6.0)
 # Robotology projects
 set_tag(YCM_TAG ycm-0.11)
 set_tag(YARP_TAG yarp-3.4)
-set_tag(YARP_TAG 0328577b5f102da2875517898d8a6439ac9374d2)
 set_tag(yarp-matlab-bindings_TAG yarp-3.4)
