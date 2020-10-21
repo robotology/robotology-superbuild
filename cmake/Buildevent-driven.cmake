bfaabdcfe0aaaa2a779099ee6f0b7411a6c8e5c4
francodipietro <francodipietro89@gmail.com>
2020-10-21 09:52:54 +0200

    Added YCM dependency for event-driven repo and updated ci.yml

--- cmake/Buildevent-driven.cmake
+++ cmake/Buildevent-driven.cmake
@@ -5,10 +5,12 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
+find_or_build_package(YCM QUIET)
 
 ycm_ep_helper(event-driven TYPE GIT
               STYLE GITHUB
               REPOSITORY robotology/event-driven.git
-              DEPENDS YARP
+              DEPENDS	YARP
+              	      	YCM
               COMPONENT event-driven
               FOLDER robotology)
