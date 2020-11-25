7e6f6041f5d3f65d4e37f4eda19aa1f6a29a7218
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-11-25 22:45:29 +0100

    Implement Buildmatio-cpp.cmake file

---
+++ cmake/Buildmatio-cpp.cmake
@@ -0,0 +1,12 @@
+# Copyright (C) 2020  iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Giulio Romualdi <giulio.romualdi@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+include(YCMEPHelper)
+
+ycm_ep_helper(matio-cpp TYPE GIT
+              STYLE GITHUB
+              REPOSITORY dic-iit/matio-cpp.git
+              TAG master
+              COMPONENT dynamics
+              FOLDER robotology
+              CMAKE_ARGS -DBUILD_TESTING:BOOL=OFF)
