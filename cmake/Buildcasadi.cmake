a97dab519952e9e955ec95e86d68f07feb020a25
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-11-26 10:58:08 +0100

    Make Buildcasadi.cmake compatible with Windows

--- cmake/Buildcasadi.cmake
+++ cmake/Buildcasadi.cmake
@@ -13,5 +13,11 @@ ycm_ep_helper(casadi TYPE GIT
               TAG feature/support_osqp_0.6.0
               COMPONENT external
               FOLDER external
-              CMAKE_ARGS -DWITH_IPOPT:BOOL=ON -DWITH_OSQP:BOOL=ON -DUSE_SYSTEM_WISE_OSQP:BOOL=ON
+              CMAKE_ARGS -DWITH_IPOPT:BOOL=ON
+                         -DWITH_OSQP:BOOL=ON
+                         -DUSE_SYSTEM_WISE_OSQP:BOOL=ON
+                         -DINCLUDE_PREFIX:PATH="include"
+                         -DCMAKE_PREFIX:PATH="lib/cmake/casadi"
+                         -DLIB_PREFIX:PATH="lib"
+                         -DBIN_PREFIX:PATH="bin"
               DEPENDS osqp)
