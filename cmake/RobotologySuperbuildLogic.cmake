48b3c6b9111ff7a69947ec90f63a92cdb9e6c6bd
Silvio Traversaro <silvio.traversaro@iit.it>
2020-12-05 11:53:33 +0100

    Cleanup CMake logic
    Create two new files:
    * RobotologySuperbuildOptions that contain the options of the superbuild
    * RobotologySuperbuildLogic that contain the logic of which projects need
      to be built (and their cmake options) depending on options enabled.

---
+++ cmake/RobotologySuperbuildLogic.cmake
@@ -0,0 +1,105 @@
+# Copyright (C) Fondazione Istituto Italiano di Tecnologia
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+# Core
+if(ROBOTOLOGY_ENABLE_CORE)
+  find_or_build_package(YARP)
+  find_or_build_package(ICUB)
+  find_or_build_package(ICUBcontrib)
+  find_or_build_package(icub-models)
+  find_or_build_package(robots-configuration)
+  if(ROBOTOLOGY_USES_GAZEBO)
+    find_or_build_package(GazeboYARPPlugins)
+    find_or_build_package(icub-gazebo)
+  endif()
+  if(ROBOTOLOGY_USES_MATLAB OR ROBOTOLOGY_USES_OCTAVE)
+    find_or_build_package(yarp-matlab-bindings)
+  endif()
+endif()
+
+# Robot Testing
+if(ROBOTOLOGY_ENABLE_ROBOT_TESTING)
+  find_or_build_package(RobotTestingFramework)
+  find_or_build_package(icub-tests)
+  find_or_build_package(blocktestcore)
+  find_or_build_package(blocktest-yarp-plugins)
+endif()
+
+# Dynamics
+if(ROBOTOLOGY_ENABLE_DYNAMICS)
+  find_or_build_package(iDynTree)
+  find_or_build_package(qpOASES)
+  find_or_build_package(BlockFactory)
+  find_or_build_package(WBToolbox)
+  find_or_build_package(osqp)
+  find_or_build_package(OsqpEigen)
+  find_or_build_package(UnicyclePlanner)
+  find_or_build_package(walking-controllers)
+  find_or_build_package(whole-body-estimators)
+  find_or_build_package(whole-body-controllers)
+  find_or_build_package(matio-cpp)
+  find_or_build_package(bipedal-locomotion-framework)
+  if(ROBOTOLOGY_USES_GAZEBO)
+    find_or_build_package(icub-gazebo-wholebody)
+  endif()
+endif()
+
+# Teleoperation
+if(ROBOTOLOGY_ENABLE_TELEOPERATION)
+  find_or_build_package(walking-teleoperation)
+endif()
+
+# Human Dynamics Estimation
+if(ROBOTOLOGY_ENABLE_HUMAN_DYNAMICS)
+  find_or_build_package(forcetorque-yarp-devices)
+  find_or_build_package(wearables)
+  find_or_build_package(human-dynamics-estimation)
+  find_or_build_package(human-gazebo)
+endif()
+
+# Grasping
+if(ROBOTOLOGY_ENABLE_GRASPING)
+  find_or_build_package(icub-grasp-demo)
+endif()
+
+# Event-driven
+if(ROBOTOLOGY_ENABLE_EVENT_DRIVEN)
+  find_or_build_package(event-driven)
+endif()
+
+# IOL
+if(ROBOTOLOGY_ENABLE_IOL)
+  if(NOT ROBOTOLOGY_USES_LUA)
+    message(FATAL_ERROR "Impossible to set ROBOTOLOGY_ENABLE_IOL to TRUE if ROBOTOLOGY_USES_LUA is set to false")
+  endif()
+  find_or_build_package(iol)
+endif()
+
+# iCub Robot
+if(ROBOTOLOGY_ENABLE_ICUB_HEAD)
+  find_or_build_package(icub_firmware_shared)
+  find_or_build_package(ICUB)
+  if((ROBOTOLOGY_PROJECT_TAGS STREQUAL "Unstable") AND (NOT APPLE))
+    find_or_build_package(diagnosticdaemon)
+  endif()
+  if((NOT WIN32) AND (NOT APPLE))
+    find_or_build_package(yarp-device-xsensmt)
+  endif()
+endif()
+
+# iCub Basic Demos
+if(ROBOTOLOGY_ENABLE_ICUB_BASIC_DEMOS)
+  find_or_build_package(icub-basic-demos)
+  find_or_build_package(speech)
+  find_or_build_package(funny-things)
+endif()
+
+# R1 Robot
+if(ROBOTOLOGY_ENABLE_R1_ROBOT)
+  find_or_build_package(navigation)
+  find_or_build_package(cer)
+endif()
+
+if(ROBOTOLOGY_USES_OCULUS_SDK)
+  find_or_build_package(yarp-device-ovrheadset)
+endif()
---
+++ cmake/RobotologySuperbuildOptions.cmake
@@ -0,0 +1,90 @@
+# Copyright (C) Fondazione Istituto Italiano di Tecnologia
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+set(YCM_USE_CMAKE_PROPOSED TRUE CACHE BOOL "Use files including unmerged cmake patches")
+
+# YCM options
+option(YCM_DISABLE_SYSTEM_PACKAGES "Disable use of all the system installed packages" ON)
+
+# Dependencies options
+## Matlab related options
+option(ROBOTOLOGY_USES_MATLAB "Enable compilation of software that depend on Matlab and Simulink" FALSE)
+option(ROBOTOLOGY_NOT_USE_SIMULINK "Disable compilation of software that depend on Simulink" FALSE)
+
+## Other dynamic languages options
+option(ROBOTOLOGY_USES_OCTAVE "Enable compilation of software that depend on Octave" FALSE)
+option(ROBOTOLOGY_USES_LUA "Enable compilation of software that depend on Lua" FALSE)
+mark_as_advanced(ROBOTOLOGY_USES_LUA)
+option(ROBOTOLOGY_USES_PYTHON "Enable compilation of software that depend on Python" FALSE)
+
+## Find the active version of Python
+find_package(Python COMPONENTS Interpreter)
+set(PYTHON_VERSION_PATH "python${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}")
+
+## Enable packages that depend on the Gazebo simulator
+if(WIN32)
+  set(ROBOTOLOGY_USES_GAZEBO_DEFAULT FALSE)
+else()
+  set(ROBOTOLOGY_USES_GAZEBO_DEFAULT TRUE)
+endif()
+option(ROBOTOLOGY_USES_GAZEBO "Enable compilation of software that depends on Gazebo" ${ROBOTOLOGY_USES_GAZEBO_DEFAULT})
+
+## Enable Oculus SDK and Cyberith treadmill options
+option(ROBOTOLOGY_USES_OCULUS_SDK "Enable compilation of software that depend on Oculus SDK" FALSE)
+option(ROBOTOLOGY_USES_CYBERITH_SDK "Enable compilation of software that depend on Cyberith SDK" FALSE)
+
+# Enable/disable different profiles
+option(ROBOTOLOGY_ENABLE_CORE "Enable compilation of core software libraries." TRUE)
+option(ROBOTOLOGY_ENABLE_ROBOT_TESTING "Enable compilation of software for robot testing." FALSE)
+option(ROBOTOLOGY_ENABLE_DYNAMICS "Enable compilation of software for balancing and walking." FALSE)
+option(ROBOTOLOGY_ENABLE_DYNAMICS_FULL_DEPS "Enable compilation of all the dependencies of bipedal-locomotion-framework." FALSE)
+option(ROBOTOLOGY_ENABLE_TELEOPERATION "Enable compilation of software for teleoperation." FALSE)
+option(ROBOTOLOGY_ENABLE_EVENT_DRIVEN "Enable compilation of software for event-driven." FALSE)
+option(ROBOTOLOGY_ENABLE_ICUB_HEAD "Enable compilation of software necessary on the system running in the head of the iCub robot." FALSE)
+option(ROBOTOLOGY_ENABLE_ICUB_BASIC_DEMOS "Enable compilation of software necessary for iCub basic demonstrations." FALSE)
+option(ROBOTOLOGY_ENABLE_HUMAN_DYNAMICS "Enable compilation of software for human dynamics estimation." FALSE)
+include(CMakeDependentOption)
+
+cmake_dependent_option(ROBOTOLOGY_USES_CFW2CAN "Enable compilation of software that runs on the head of the iCub and depends on CFW2CAN." FALSE "ROBOTOLOGY_ENABLE_ICUB_HEAD" FALSE)
+cmake_dependent_option(ROBOTOLOGY_USES_ESDCAN "Enable compilation of software that runs on the head of the iCub and depends on ESDCAN." FALSE "ROBOTOLOGY_ENABLE_ICUB_HEAD" FALSE)
+cmake_dependent_option(ROBOTOLOGY_USES_XSENS_MVN_SDK "Enable compilation of software that runs on the wearable producer machine and depends on Xsens MVN SDK." FALSE "ROBOTOLOGY_ENABLE_HUMAN_DYNAMICS" FALSE)
+
+# Mark as advanced all the profiles that do not have a mantainer
+option(ROBOTOLOGY_ENABLE_GRASPING "Enable compilation of software for grasping." FALSE)
+mark_as_advanced(ROBOTOLOGY_ENABLE_GRASPING)
+option(ROBOTOLOGY_ENABLE_IOL "Enable compilation of software necessary for the Interactive Objects Learning demo." FALSE)
+mark_as_advanced(ROBOTOLOGY_ENABLE_IOL)
+option(ROBOTOLOGY_ENABLE_R1_ROBOT "Enable compilation of software necessary on the pc running on the R1 robot." FALSE)
+mark_as_advanced(ROBOTOLOGY_ENABLE_R1_ROBOT)
+
+#set default build type to "Release" in single-config generators
+if(NOT CMAKE_CONFIGURATION_TYPES)
+    if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Release" CACHE STRING
+        "Choose the type of build, recommanded options are: Debug or Release" FORCE)
+    endif()
+    set(ROBOTOLOGY_BUILD_TYPES "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${ROBOTOLOGY_BUILD_TYPES})
+endif()
+
+set(ROBOTOLOGY_PROJECT_TAGS "Stable" CACHE STRING "The tags to be used for the robotology projects: Stable, Unstable or Custom. This can be changed only before the first configuration.")
+mark_as_advanced(ROBOTOLOGY_PROJECT_TAGS)
+set(ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE CACHE FILEPATH "If ROBOTOLOGY_PROJECT_TAGS is custom, this file will be loaded to specify the tags of the projects to use.")
+mark_as_advanced(ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE)
+set_property(CACHE ROBOTOLOGY_PROJECT_TAGS PROPERTY STRINGS "Stable" "Unstable" "Custom")
+
+if(ROBOTOLOGY_PROJECT_TAGS STREQUAL "Stable")
+    include(ProjectsTagsStable)
+elseif(ROBOTOLOGY_PROJECT_TAGS STREQUAL "Unstable")
+    include(ProjectsTagsUnstable)
+elseif(ROBOTOLOGY_PROJECT_TAGS STREQUAL "Custom")
+    if(ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE MATCHES ".yaml$" OR
+       ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE MATCHES ".repos$")
+       include(YCMLoadVcsYamlInfo)
+       ycm_load_vcs_yaml_info(YAML_FILE ${ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE} VERBOSE)
+    else()
+        include(${ROBOTOLOGY_PROJECT_TAGS_CUSTOM_FILE})
+    endif()
+else()
+    message(FATAL_ERROR "The ROBOTOLOGY_PROJECT_TAGS variable can be Stable, Unstable or Custom. ${ROBOTOLOGY_PROJECT_TAGS} value is not supported.")
+endif()
