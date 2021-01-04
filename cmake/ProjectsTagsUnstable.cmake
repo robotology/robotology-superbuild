d6bf2212c5a057ee8485c97a3c480afd305d6b19
Silvio Traversaro <silvio.traversaro@iit.it>
2021-01-04 09:23:54 +0100

    Update ProjectsTagsUnstable.cmake

--- cmake/ProjectsTagsUnstable.cmake
+++ cmake/ProjectsTagsUnstable.cmake
@@ -8,7 +8,7 @@ endmacro()
 set_tag(osqp_TAG v0.6.0)
 set_tag(manif_TAG 44bdfebff0fbc56cb189f680212257dc7f20ea58)
 set_tag(qhull_TAG v8.0.2)
-set_tag(CppAD_TAG 4aac52664de911af02cfade06131a9a6f6b48d7e)
+set_tag(CppAD_TAG 20210000.1)
 set_tag(casadi a26cd8ffba99052b74553eec1daeff640eea7e79)
 
 # Robotology projects
