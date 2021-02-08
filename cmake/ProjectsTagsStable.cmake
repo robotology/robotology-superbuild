80f8933c3a0b5b05fe1f5e7b125c31f2d1a71ed3
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-02-08 15:13:12 +0100

    Bump the version of CppAD

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -8,7 +8,7 @@ endmacro()
 set_tag(osqp_TAG v0.6.0)
 set_tag(manif_TAG 44bdfebff0fbc56cb189f680212257dc7f20ea58)
 set_tag(qhull_TAG v8.0.2)
-set_tag(CppAD_TAG 20210000.2)
+set_tag(CppAD_TAG 72fde5fc783c9d85a6d2453631f0b8c3f4159fe1)
 set_tag(casadi a26cd8ffba99052b74553eec1daeff640eea7e79)
 
 # Robotology projects
