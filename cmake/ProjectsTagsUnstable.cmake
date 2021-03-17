b4fdebf66515d358dce93296a7e8e6df90c0b81b
Silvio Traversaro <silvio@traversaro.it>
2021-03-17 21:30:03 +0100

    Update ProjectsTagsUnstable.cmake

--- cmake/ProjectsTagsUnstable.cmake
+++ cmake/ProjectsTagsUnstable.cmake
@@ -6,7 +6,7 @@ endmacro()
 
 # External projects
 set_tag(osqp_TAG v0.6.2)
-set_tag(manif_TAG 0.0.3)
+set_tag(manif_TAG 85cad9841b0ac4800e10e38c634c397d401e6375)
 set_tag(qhull_TAG v8.0.2)
 set_tag(CppAD_TAG 20210000.4)
 set_tag(casadi 3.5.5.2)
