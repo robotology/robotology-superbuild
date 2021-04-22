466c71669e652ed4b4c8492a16a31a35c76f7ac4
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-04-22 14:30:08 +0200

    Fix typo in Buildcasadi.cmake

--- cmake/Buildcasadi.cmake
+++ cmake/Buildcasadi.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2020  iCub Facility, Istituto Italiano di Tecnologia
-# Authors: Giulio Romualdi <gulio.romualdi@iit.it>
+# Authors: Giulio Romualdi <giulio.romualdi@iit.it>
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 
 include(YCMEPHelper)
