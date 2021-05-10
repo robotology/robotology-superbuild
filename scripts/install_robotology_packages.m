903d95cca34e30edef20ac132024df7497f2740c
Silvio Traversaro <silvio.traversaro@iit.it>
2021-05-10 15:06:15 +0200

    Update install_robotology_packages.m

--- scripts/install_robotology_packages.m
+++ scripts/install_robotology_packages.m
@@ -15,7 +15,8 @@ function install_robotology_packages(varargin)
         return;
     end
 
-    fprintf('Installing robotology MATLAB/Simulink binaries in %s', install_prefix);
+    fprintf('Installing robotology MATLAB/Simulink binaries in %s\n', install_prefix);
+
     % The install url is created following 
     mambaforge_url_prefix = 'https://github.com/conda-forge/miniforge/releases/latest/download/';
     if ispc
@@ -34,25 +35,38 @@ function install_robotology_packages(varargin)
         uname_m = strip(uname_m);
         mambaforge_installer_name = sprintf('Mambaforge-%s-%s.sh', uname, uname_m);
     end
+    
+    fprintf('Downloading mambaforge installer \n');
     mambaforge_installer_url = strcat(mambaforge_url_prefix, mambaforge_installer_name);
     websave(mambaforge_installer_name, mambaforge_installer_url);
-    
+    fprintf('Download of mambaforge installer completed\n');
+
+    % See https://github.com/conda-forge/miniforge#non-interactive-install
+    fprintf('Installing mambaforge\n');
     if ispc
-        system(sprintf('TODO'));
-        mamba_full_path = 'TODO';
+        system(sprintf('start /wait "" %s /InstallationType=JustMe /RegisterPython=0 /S /D=%s', mambaforge_installer_name, install_prefix));
+        conda_full_path = fullfile(install_prefix, 'condabin', 'conda.bat');
+        % On Windows, the files in conda are installed in the Library
+        % subdirectory of the prefix
+        robotology_install_prefix = fullfile(install_prefix, 'Library');
     elseif isunix
         system(sprintf('sh %s -b -p %s', mambaforge_installer_name, install_prefix));
-        mamba_full_path = fullfile(install_prefix, 'bin', 'mamba');
+        conda_full_path = fullfile(install_prefix, 'bin', 'conda');
+        robotology_install_prefix = install_prefix;
     end
+    fprintf('Installation of mambaforge completed\n');
     
+    
+
     if ~exist(install_prefix, 'dir')
-        fprintf('Installation in %s failed for unknown reason, please open an issue at https://github.com/robotology/robotology-superbuild/issues/new', install_prefix);
+        fprintf('Installation in %s failed for unknown reason, please open an issue at https://github.com/robotology/robotology-superbuild/issues/new\n', install_prefix);
         return;
     end
     
     % Install all the robotology packages related to MATLAB or Simulink
-    system(sprintf('%s install -y -c conda-forge -c robotology yarp-matlab-bindings idyntree wb-toolbox osqp-matlab whole-body-controllers matlab-whole-body-simulator icub-models', mamba_full_path));    
-    fprintf('\t\t\t\t[done]\n');
+    fprintf('Installing robotology packages\n');
+    system(sprintf('%s install -y -c conda-forge -c robotology yarp-matlab-bindings idyntree wb-toolbox osqp-matlab whole-body-controllers matlab-whole-body-simulator icub-models', conda_full_path));    
+    fprintf('Installation of robotology packages completed\n');
 
     fprintf('Creating setup script in %s', setup_script);
     % Generate robotology_setup.m 
@@ -67,7 +81,7 @@ function install_robotology_packages(varargin)
     fprintf(setupID, 'end\n');
     fprintf(setupID, '\n');
     fprintf(setupID, '%% Install prefix (hardcoded at generation time)\n');
-    fprintf(setupID, 'robotology_install_prefix = "%s";\n', install_prefix);
+    fprintf(setupID, 'robotology_install_prefix = "%s";\n', robotology_install_prefix);
     fprintf(setupID, '\n');
     fprintf(setupID, '%% Add directory to MATLAB path\n');
     fprintf(setupID, 'addpath(fullfile(robotology_install_prefix,"mex"));\n');
@@ -84,13 +98,12 @@ function install_robotology_packages(varargin)
     fprintf(setupID, 'setenv("ROS_PACKAGE_PATH",strcat(fullfile(robotology_install_prefix,"share"), rob_env_sep, getenv("ROS_PACKAGE_PATH")));\n');
     fprintf(setupID, 'setenv("BLOCKFACTORY_PLUGIN_PATH",fullfile(robotology_install_prefix,rob_shlib_install_dir,"blockfactory"));\n');
     fclose(setupID);
-    fprintf('\t\t\t\t[done]\n');
 
-    fprintf('Deleting temporary files...');
+    fprintf('Deleting mambaforge installer');
     delete(mambaforge_installer_name);
-    fprintf('\t\t\t[done]\n');
 
     fprintf('robotology MATLAB and Simulink packages are successfully installed!\n');
-    fprintf('Please run %s before using the packages, or just add that script to your startup.m file, to run it whenever you open MATLAB.\n', setup_script);
+    fprintf('Please run %s before using the packages,\n',setup_script)
+    fprintf('or just add that script to your startup.m file, to run it whenever you open MATLAB.\n');
     fprintf('To uninstall these packages, just delete the folder %s.\n', install_prefix);
 end
