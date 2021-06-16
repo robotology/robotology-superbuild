1ff3170701693c28f64791810b13162fa5f3f435
Silvio Traversaro <silvio.traversaro@iit.it>
2021-06-16 15:20:26 +0200

    Bump manif version to 0.0.4 and blf to v0.2.0 (#798)

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -6,7 +6,7 @@ endmacro()
 
 # External projects
 set_tag(osqp_TAG v0.6.2)
-set_tag(manif_TAG 0.0.3)
+set_tag(manif_TAG 0.0.4)
 set_tag(qhull_TAG v8.0.2)
 set_tag(CppAD_TAG 20210000.4)
 set_tag(casadi 3.5.5.3)
@@ -15,6 +15,3 @@ set_tag(casadi 3.5.5.3)
 set_tag(YCM_TAG ycm-0.12)
 set_tag(YARP_TAG yarp-3.4)
 set_tag(yarp-matlab-bindings_TAG yarp-3.4)
-
-# Temporary workaround for manif incompatibiltiy
-set_tag(bipedal-locomotion-framework_TAG 5b76dc40b0f9e825e29f8de5e100240b9f7d640c)
