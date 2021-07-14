69457c83dbcc83812bb47ed87540e6885fc308ac
Silvio Traversaro <silvio.traversaro@iit.it>
2021-07-14 14:49:39 +0200

    Update install_apt_dependencies.sh

--- scripts/install_apt_dependencies.sh
+++ scripts/install_apt_dependencies.sh
@@ -6,4 +6,4 @@
 # Get location of the script
 SCRIPT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; cd -P "$(dirname "$(readlink "$BASH_SOURCE" || echo .)")"; pwd)
 
-xargs -a ${SCRIPT_DIR}/../apt.txt apt-get install
+xargs -a ${SCRIPT_DIR}/../apt.txt apt-get install -y
