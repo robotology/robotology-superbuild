9b4e9bb850ed435efc84d49b3177eeed28afcb9f
Silvio Traversaro <silvio.traversaro@iit.it>
2021-08-25 09:48:30 +0200

    Update install_apt_dependencies.sh

--- scripts/install_apt_dependencies.sh
+++ scripts/install_apt_dependencies.sh
@@ -3,12 +3,12 @@
 # Install dependencies of robotology-superbuild 
 # using apt on Ubuntu or Debian 
 
-lsb_release -a
-
 # Get location of the script
 SCRIPT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; cd -P "$(dirname "$(readlink "$BASH_SOURCE" || echo .)")"; pwd)
 
 xargs -a ${SCRIPT_DIR}/../apt.txt apt-get install -y
+
+lsb_release -a
 
 # Handle libdc1394 package (see https://github.com/robotology/robotology-superbuild/issues/854)
 # On Ubuntu 18.04 or Debian Buster install libdc1394-22-dev, otherwise libdc1394-dev
