40f0086063642f9c2bcbadc610bd0df345baf125
Silvio Traversaro <silvio.traversaro@iit.it>
2021-08-25 09:59:37 +0200

    Update install_apt_dependencies.sh

--- scripts/install_apt_dependencies.sh
+++ scripts/install_apt_dependencies.sh
@@ -8,8 +8,6 @@ SCRIPT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; cd -P "$(dirname "$(readlink "$BASH
 
 xargs -a ${SCRIPT_DIR}/../apt.txt apt-get install -y
 
-lsb_release -a
-
 # Handle libdc1394 package (see https://github.com/robotology/robotology-superbuild/issues/854)
 # On Ubuntu 18.04 or Debian Buster install libdc1394-22-dev, otherwise libdc1394-dev
 # Remove once Ubuntu 18.04 and Debian Buster compatibility is dropped
@@ -17,7 +15,7 @@ ROBSUP_DISTRO_NAME=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
 ROBSUP_DISTRO_VERSION=$(lsb_release -r | cut -d: -f2 | sed s/'^\t'//)
 ROBSUP_DISTRO_CODENAME=$(lsb_release -c | cut -d: -f2 | sed s/'^\t'//)
 
-if [[ ("$ROBSUP_DISTRO_NAME" == "Ubuntu" && "$ROBSUP_DISTRO_VERSION" == "22.04") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bookworm") ]]
+if [[ ("$ROBSUP_DISTRO_NAME" == "Ubuntu" && "$ROBSUP_DISTRO_VERSION" == "22.04") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bullseye") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bookworm") ]]
 then
   apt-get install -y libdc1394-dev
 else
