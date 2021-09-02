795ba3d2f32787a0c07c996b40fa1c950b6e0cc2
Silvio Traversaro <silvio.traversaro@iit.it>
2021-09-02 17:57:00 +0200

    Update install_debian.sh

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -26,7 +26,7 @@ lsb_dist="$(. /etc/os-release && echo "$ID")"
 dist_version="$(. /etc/os-release && echo "$VERSION_CODENAME")"
 # bullseye is not supported by OpenRobotics' repo, but it has already 
 # the right version of Gazebo in its repo, so we just skip everything
-if [ "sid" != "$dist_version" ]; then
+if [ "sid" != "$dist_version" &&  "bullseye" != "$dist_version" ]; then
     mkdir -p /etc/apt/sources.list.d
     echo deb http://packages.osrfoundation.org/gazebo/$lsb_dist\-stable $dist_version main > /etc/apt/sources.list.d/gazebo-stable.list
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2486D2DD83DB69272AFE98867170598AF249743
