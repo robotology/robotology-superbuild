66d94ac465fba251cb3f3bfa8e17380eda9f9289
Silvio Traversaro <silvio.traversaro@iit.it>
2021-09-03 14:57:05 +0200

    Always install Gazebo 11 in apt CI to fix Unstable CI (#873)

--- scripts/install_apt_dependencies.sh
+++ scripts/install_apt_dependencies.sh
@@ -15,7 +15,10 @@ ROBSUP_DISTRO_NAME=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
 ROBSUP_DISTRO_VERSION=$(lsb_release -r | cut -d: -f2 | sed s/'^\t'//)
 ROBSUP_DISTRO_CODENAME=$(lsb_release -c | cut -d: -f2 | sed s/'^\t'//)
 
-if [[ ("$ROBSUP_DISTRO_NAME" == "Ubuntu" && "$ROBSUP_DISTRO_VERSION" == "22.04") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bullseye") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bookworm") ]]
+echo "ROBSUP_DISTRO_NAME: ${ROBSUP_DISTRO_NAME}"
+echo "ROBSUP_DISTRO_VERSION: ${ROBSUP_DISTRO_VERSION}"
+echo "ROBSUP_DISTRO_CODENAME: ${ROBSUP_DISTRO_CODENAME}"
+if [[ ("$ROBSUP_DISTRO_NAME" == "Ubuntu" && "$ROBSUP_DISTRO_VERSION" == "22.04") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bullseye") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "bookworm") || ("$ROBSUP_DISTRO_NAME" == "Debian" && "$ROBSUP_DISTRO_CODENAME" == "sid") ]]
 then
   apt-get install -y libdc1394-dev
 else
