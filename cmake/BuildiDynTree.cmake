05163c4b84663a001751e98ec0e802e4a2d3578a
Silvio Traversaro <silvio.traversaro@iit.it>
2021-11-07 19:26:23 +0100

    Update BuildiDynTree.cmake

--- cmake/BuildiDynTree.cmake
+++ cmake/BuildiDynTree.cmake
@@ -30,6 +30,7 @@ ycm_ep_helper(iDynTree TYPE GIT
               CMAKE_ARGS -DIDYNTREE_USES_IPOPT:BOOL=ON
                          -DIDYNTREE_USES_OSQPEIGEN:BOOL=ON
                          -DIDYNTREE_USES_IRRLICHT:BOOL=ON
+                         -DIDYNTREE_USES_ASSIMP:BOOL=ON
                          -DIDYNTREE_USES_MATLAB:BOOL=${ROBOTOLOGY_USES_MATLAB}
                          -DIDYNTREE_USES_PYTHON:BOOL=${ROBOTOLOGY_USES_PYTHON}
                          -DIDYNTREE_USES_OCTAVE:BOOL=${ROBOTOLOGY_USES_OCTAVE}
