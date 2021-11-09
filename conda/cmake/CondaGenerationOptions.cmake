72a4f1c705fe6880bfb3f06515ea9836e182e709
traversaro <traversaro@users.noreply.github.com>
2021-11-09 04:00:50 +0000

    Bump CONDA_BUILD_NUMBER after successful Conda packages build and upload

--- conda/cmake/CondaGenerationOptions.cmake
+++ conda/cmake/CondaGenerationOptions.cmake
@@ -1,7 +1,7 @@
 # This number needs to be increased at each full rebuild,
 # to ensure that binaries belonging to different rebuilds
 # can be distinguished even if the version number is the same
-set(CONDA_BUILD_NUMBER 30)
+set(CONDA_BUILD_NUMBER 31)
 
 # For more conda generation options, check the specific project Build<CMakeProject>.cmake
 # file for variables that start with `<CMakeProject>_CONDA`
