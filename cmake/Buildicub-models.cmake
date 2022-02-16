061096f1c2fafcca714ab4782473beb7c43b5d24
Silvio Traversaro <silvio.traversaro@iit.it>
2022-02-16 14:19:14 +0100

    Add support for Python helper library of icub-models (#1037)

--- cmake/Buildicub-models.cmake
+++ cmake/Buildicub-models.cmake
@@ -3,10 +3,25 @@
 
 include(YCMEPHelper)
 
+# For what regards Python installation, the options changes depending
+# on whater we are installing blf in the superbuild, or we are generating a
+# conda package on Windows as in that case the installation location
+# will need to be outside of CMAKE_INSTALL_PREFIX
+# See https://github.com/robotology/robotology-superbuild/issues/641
+set(icub-models_OPTIONAL_CMAKE_ARGS "")
+if(ROBOTOLOGY_USES_PYTHON AND ROBOTOLOGY_GENERATE_CONDA_RECIPES)
+  list(APPEND icub-models-framework_OPTIONAL_CMAKE_ARGS "-DICUB_MODELS_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON")
+endif()
+
 ycm_ep_helper(icub-models
               TYPE GIT
               STYLE GITHUB
               REPOSITORY robotology/icub-models.git
               TAG master
               COMPONENT iCub
-              FOLDER src)
+              FOLDER src
+              CMAKE_ARGS ${icub-models_OPTIONAL_CMAKE_ARGS})
+
+if(ROBOTOLOGY_USES_PYTHON)
+  set(icub-models_CONDA_DEPENDENCIES pybind11)
+endif()
