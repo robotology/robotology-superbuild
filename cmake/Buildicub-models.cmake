7f585e88e0b0b3514c63565f237f7d5a510d1a15
Silvio Traversaro <silvio.traversaro@iit.it>
2022-03-04 17:31:38 +0100

    Update Buildicub-models.cmake

--- cmake/Buildicub-models.cmake
+++ cmake/Buildicub-models.cmake
@@ -10,9 +10,6 @@ include(YCMEPHelper)
 # See https://github.com/robotology/robotology-superbuild/issues/641
 set(icub-models_OPTIONAL_CMAKE_ARGS "")
 list(APPEND icub-models_OPTIONAL_CMAKE_ARGS "-DICUB_MODELS_COMPILE_PYTHON_BINDINGS:BOOL=${ROBOTOLOGY_USES_PYTHON}")
-if(ROBOTOLOGY_USES_PYTHON AND ROBOTOLOGY_GENERATE_CONDA_RECIPES)
-  list(APPEND icub-models_OPTIONAL_CMAKE_ARGS "-DICUB_MODELS_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON")
-endif()
 
 ycm_ep_helper(icub-models
               TYPE GIT
@@ -23,7 +20,5 @@ ycm_ep_helper(icub-models
               FOLDER src
               CMAKE_ARGS ${icub-models_OPTIONAL_CMAKE_ARGS})
 
-if(ROBOTOLOGY_USES_PYTHON)
-  set(icub-models_CONDA_DEPENDENCIES python)
-  list(APPEND icub-models_CONDA_DEPENDENCIES pybind11)
-endif()
+set(icub-models_CONDA_PKG_NAME icub-models)
+set(icub-models_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
