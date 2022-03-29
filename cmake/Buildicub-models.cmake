d62e3a3e6f6c9cf3c2a6ba1d080ed394f82435e2
Silvio Traversaro <silvio.traversaro@iit.it>
2022-03-29 14:27:53 +0200

    Fix support for Python bindings in icub-models
    After the change of options in https://github.com/robotology/icub-models/pull/143

--- cmake/Buildicub-models.cmake
+++ cmake/Buildicub-models.cmake
@@ -3,13 +3,9 @@
 
 include(YCMEPHelper)
 
-# For what regards Python installation, the options changes depending
-# on whater we are installing blf in the superbuild, or we are generating a
-# conda package on Windows as in that case the installation location
-# will need to be outside of CMAKE_INSTALL_PREFIX
-# See https://github.com/robotology/robotology-superbuild/issues/641
 set(icub-models_OPTIONAL_CMAKE_ARGS "")
 list(APPEND icub-models_OPTIONAL_CMAKE_ARGS "-DICUB_MODELS_COMPILE_PYTHON_BINDINGS:BOOL=${ROBOTOLOGY_USES_PYTHON}")
+list(APPEND icub-models_OPTIONAL_CMAKE_ARGS "-DICUB_MODELS_USES_PYTHON:BOOL=${ROBOTOLOGY_USES_PYTHON}")
 
 ycm_ep_helper(icub-models
               TYPE GIT
