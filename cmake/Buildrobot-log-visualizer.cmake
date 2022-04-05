cd5144fa3c3d285b6a67f3b88a4ccc21f89f8227
Silvio Traversaro <silvio.traversaro@iit.it>
2022-04-05 14:49:38 +0200

    Update Buildrobot-log-visualizer.cmake

--- cmake/Buildrobot-log-visualizer.cmake
+++ cmake/Buildrobot-log-visualizer.cmake
@@ -6,12 +6,14 @@ include(RobSupPurePythonYCMEPHelper)
 find_or_build_package(icub-models QUIET)
 find_or_build_package(pyqtconsole QUIET)
 find_or_build_package(meshcat-python QUIET)
+find_or_build_package(iDynTree QUIET)
 
 rob_sup_pure_python_ycm_ep_helper(robot-log-visualizer
                                   REPOSITORY ami-iit/robot-log-visualizer.git
                                   DEPENDS meshcat-python
                                           pyqtconsole
                                           icub-models
+                                          iDynTree
                                   TAG main
                                   COMPONENT dynamics
                                   FOLDER src)
