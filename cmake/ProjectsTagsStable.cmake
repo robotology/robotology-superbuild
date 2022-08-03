af4b7fdcd2b0fe8c8ddb3fe71527265e40b6d313
Nicolò Genesio (二コゲネ) <nicolo.genesio@iit.it>
2022-08-03 17:28:07 +0200

    Remove YARP_telemetry
    It has been deprecated in favour of robometry

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -17,5 +17,5 @@ set_tag(YCM_TAG ycm-0.14)
 set_tag(YARP_TAG yarp-3.7)
 set_tag(yarp-matlab-bindings_TAG yarp-3.7)
 set_tag(gym-ignition_TAG v1.3.1)
-set_tag(YARP_telemetry_TAG v0.5.1)
+set_tag(robometry_TAG v1.1.0)
 
