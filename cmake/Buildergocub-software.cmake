abeadbd202c70c98e60822ec3ba400d6d1347aa4
Nicogene <nicolo.genesio@iit.it>
2023-01-25 09:56:13 +0100

    Add ergocub-software to core profile

---
+++ cmake/Buildergocub-software.cmake
@@ -0,0 +1,18 @@
+# Copyright (C) 2023 iCub Tech, Istituto Italiano di Tecnologia
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+
+set(ergocub-software_OPTIONAL_CMAKE_ARGS "")
+
+ycm_ep_helper(ergocub-software
+              TYPE GIT
+              STYLE GITHUB
+              REPOSITORY icub-tech-iit/ergocub-software.git
+              TAG master
+              COMPONENT core
+              FOLDER src
+              CMAKE_ARGS ${ergocub-software_OPTIONAL_CMAKE_ARGS})
+
+set(ergocub-software_CONDA_PKG_NAME ergocub-software)
+set(ergocub-software_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
