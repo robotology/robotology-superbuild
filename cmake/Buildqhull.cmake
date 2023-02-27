8cb85a34b58237293dd21f2ca136961364ca6e18
Silvio Traversaro <silvio.traversaro@iit.it>
2023-02-27 18:12:03 +0100

    Create Buildqhull.cmake

---
+++ cmake/Buildqhull.cmake
@@ -0,0 +1,16 @@
+# Copyright (C) 2020 iCub Facility, Istituto Italiano di Tecnologia
+# Authors: Giulio Romualdi <gulio.romualdi@iit.it>
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+
+ycm_ep_helper(qhull TYPE GIT
+              STYLE GITHUB
+              REPOSITORY qhull/qhull.git
+              TAG master
+              COMPONENT external
+              FOLDER src
+              CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
+
+set(qhull_CONDA_PKG_NAME qhull)
+set(qhull_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
