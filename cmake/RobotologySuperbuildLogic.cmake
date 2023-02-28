137a43e2e860c9b43d01489e628296e47162b547
Silvio Traversaro <silvio.traversaro@iit.it>
2023-02-28 14:18:23 +0100

    Update RobotologySuperbuildLogic.cmake

--- cmake/RobotologySuperbuildLogic.cmake
+++ cmake/RobotologySuperbuildLogic.cmake
@@ -12,17 +12,21 @@ endif()
 # We only build qhull on Ubuntu 20.04, not on any other platform
 # See https://github.com/robotology/robotology-superbuild/issues/1269#issuecomment-1257811559
 # See https://stackoverflow.com/questions/26919334/detect-underlying-platform-flavour-in-cmake
-find_program(ROBSUB_LSB_RELEASE lsb_release)
-if(ROBSUB_LSB_RELEASE)
-  execute_process(COMMAND lsb_release -cs
-    OUTPUT_VARIABLE LSB_RELEASE_CODENAME
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-endif()
-if(ROBSUB_LSB_RELEASE STREQUAL "focal")
-  set(ROBOTOLOGY_BUILD_QHULL ON)
-else()
+if(ROBOTOLOGY_CONFIGURING_UNDER_CONDA OR APPLE OR WIN32)
   set(ROBOTOLOGY_BUILD_QHULL OFF)
+else()
+  find_program(ROBSUB_LSB_RELEASE lsb_release)
+  if(ROBSUB_LSB_RELEASE)
+    execute_process(COMMAND lsb_release -cs
+      OUTPUT_VARIABLE LSB_RELEASE_CODENAME
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  endif()
+  if(ROBSUB_LSB_RELEASE STREQUAL "focal")
+    set(ROBOTOLOGY_BUILD_QHULL ON)
+  else()
+    set(ROBOTOLOGY_BUILD_QHULL OFF)
+  endif()
 endif()
 
 # Core
