4e9c74151e5a55b0121b4daf1a7c23fd636c1995
Silvio Traversaro <silvio@traversaro.it>
2023-07-04 16:46:48 +0200

    Bump proxsuite to 0.3.7

--- cmake/ProjectsTagsUnstable.cmake
+++ cmake/ProjectsTagsUnstable.cmake
@@ -10,7 +10,7 @@ set_tag(manif_REPOSITORY robotology-dependencies/manif.git)
 set_tag(manif_TAG 0.0.4.102)
 set_tag(qhull_TAG 2020.2)
 set_tag(CppAD_TAG 20230000.0)
-set_tag(proxsuite_TAG v0.3.6)
+set_tag(proxsuite_TAG v0.3.7)
 set_tag(casadi_TAG 3.6.3)
 set_tag(casadi-matlab-bindings_TAG v3.6.3.0)
 
