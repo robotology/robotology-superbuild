4d24d5aa11b6f590d6d2b0c77d7c55facffaf501
Silvio Traversaro <silvio.traversaro@iit.it>
2024-07-13 12:50:13 +0200

    Update ProjectsTagsStable.cmake

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -15,7 +15,7 @@ set_tag(casadi_TAG 3.6.5.backport3724)
 set_tag(casadi-matlab-bindings_TAG v3.6.5.0)
 
 # Robotology projects
-set_tag(YCM_TAG 0d6b1a5a0f14c6334acd1d2b8ae3efceb51fe56c)
+set_tag(YCM_TAG fix50)
 set_tag(YARP_TAG yarp-3.9)
 set_tag(yarp-matlab-bindings_TAG yarp-3.9)
 set_tag(gym-ignition_TAG v1.3.1)
