92784d31c600e9b9eb197cd4154d13985788280e
Silvio Traversaro <silvio@traversaro.it>
2024-09-04 12:06:51 +0200

    Build Python bindings of manifpy on Windows

--- cmake/Buildmanif.cmake
+++ cmake/Buildmanif.cmake
@@ -4,13 +4,6 @@
 
 include(YCMEPHelper)
 
-# Workaround for https://github.com/robotology/robotology-superbuild/pull/838#issuecomment-887367047
-if(ROBOTOLOGY_USES_PYTHON AND NOT WIN32)
-  set(BUILD_PYTHON_BINDINGS ON)
-else()
-  set(BUILD_PYTHON_BINDINGS OFF)
-endif()
-
 # We pass CMAKE_CXX_STANDARD=17 as we encountered problems when using -march=native
 # and mixing libraries compiled with C++11 and C++17, see
 # https://github.com/artivis/manif/issues/274
@@ -20,7 +13,7 @@ ycm_ep_helper(manif TYPE GIT
               TAG master
               COMPONENT external
               FOLDER src
-              CMAKE_ARGS -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_PYTHON_BINDINGS:BOOL=${BUILD_PYTHON_BINDINGS}  -DMANIFPY_PKGDIR:PATH=${YCM_EP_INSTALL_DIR}/${ROBOTOLOGY_SUPERBUILD_PYTHON_INSTALL_DIR})
+              CMAKE_ARGS -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_PYTHON_BINDINGS:BOOL=${ROBOTOLOGY_USES_PYTHON}  -DMANIFPY_PKGDIR:PATH=${YCM_EP_INSTALL_DIR}/${ROBOTOLOGY_SUPERBUILD_PYTHON_INSTALL_DIR})
 
 set(manif_CONDA_PKG_NAME manif)
 set(manif_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
