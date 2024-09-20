20e7e3851a888140952720a4bcae239690b16365
Silvio Traversaro <silvio@traversaro.it>
2024-09-20 09:03:31 +0200

    [DO NOT MERGE] Test support-split-python-bindings branch of icub-main

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -13,6 +13,9 @@ set_tag(CppAD_TAG 20240000.2)
 set_tag(proxsuite_TAG v0.6.4)
 set_tag(casadi_TAG 3.6.6)
 set_tag(casadi-matlab-bindings_TAG v3.6.6.0)
+set_tag(ICUB_REPOSITORY traversaro/icub-main.git)
+set_tag(ICUB_TAG support-split-python-bindings)
+
 
 # Robotology projects
 set_tag(YCM_TAG master)
