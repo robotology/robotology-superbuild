8fbfd58c97cf560cd26b85698e2fc2ac1c529637
Nicolò Genesio (二コゲネ) <nicolo.genesio@iit.it>
2024-11-27 11:57:04 +0100

    Update ProjectsTagsStable.cmake in order to set yarp-3.10 as stable branch
    As per title
    
    
    cc @pattacini

--- cmake/ProjectsTagsStable.cmake
+++ cmake/ProjectsTagsStable.cmake
@@ -16,6 +16,6 @@ set_tag(casadi-matlab-bindings_TAG v3.6.7.0)
 
 # Robotology projects
 set_tag(YCM_TAG master)
-set_tag(YARP_TAG yarp-3.9)
-set_tag(yarp-matlab-bindings_TAG yarp-3.9)
+set_tag(YARP_TAG yarp-3.10)
+set_tag(yarp-matlab-bindings_TAG yarp-3.10)
 set_tag(gym-ignition_TAG v1.3.1)
