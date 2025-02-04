1a625ff58ce84f3380b8f642dedd4bc5624fc4e5
Silvio Traversaro <silvio.traversaro@iit.it>
2025-02-04 10:28:10 +0100

    Add resolve-robotics-uri-cpp C++ library to ROBOTOLOGY_ENABLE_DYNAMICS_FULL_DEPS (#1791)

---
+++ cmake/BuildResolveRoboticsURICpp.cmake
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: Fondazione Istituto Italiano di Tecnologia (IIT)
+# SPDX-License-Identifier: BSD-3-Clause
+
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(ResolveRoboticsURICpp TYPE GIT
+                                    STYLE GITHUB
+                                    REPOSITORY robotology/ycm.git
+                                    COMPONENT core
+                                    FOLDER src)
+
+set(ResolveRoboticsURICpp_CONDA_PKG_NAME libresolve-robotics-uri-cpp)
+set(ResolveRoboticsURICpp_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
