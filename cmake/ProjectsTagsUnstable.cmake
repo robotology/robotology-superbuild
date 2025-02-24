278815dbeaafc457327ff5e71af1bf4ecabc1bfb
Silvio Traversaro <silvio.traversaro@iit.it>
2025-02-24 15:57:32 +0100

    Bump YARP in Stable and Unstable branches to 3.11.1 (#1807)
    * Bump YARP in Stable and Unstable branches to 3.11.1
    
    * Use YARP 3.11 in ProjectsTagsUnstable until we fix compatibility in gazebo-yarp-plugins and gz-sim-yarp-plugins

--- cmake/ProjectsTagsUnstable.cmake
+++ cmake/ProjectsTagsUnstable.cmake
@@ -10,13 +10,12 @@ set_tag(osqp_TAG v0.6.3.1)
 set_tag(manif_TAG 0.0.5)
 set_tag(CppAD_TAG 20250000.2)
 set_tag(proxsuite_TAG v0.7.1)
-set_tag(casadi_TAG 3.6.6)
-set_tag(casadi-matlab-bindings_TAG v3.6.6.0)
+set_tag(casadi_TAG 3.6.7)
+set_tag(casadi-matlab-bindings_TAG v3.6.7.0)
 
 # Robotology projects
 # Pin YARP and yarp-devices-ros2 to a version before the yarp::dev::ReturnValue changes
-set_tag(YARP_TAG 43ba0305f8ca690cea5c2a8a6bcdcec60d3ddd0a)
-set_tag(yarp-devices-ros2_TAG e4ba8fa2efe7486edd3ade40b2236e69acb6ab37)
+set_tag(YARP_TAG yarp-3.11)
 set_tag(ICUB_TAG devel)
 set_tag(RobotTestingFramework_TAG devel)
 set_tag(blockTest_TAG devel)
