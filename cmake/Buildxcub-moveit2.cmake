c873c1f41b152ab173150f4d3b23046aa905f6b1
Silvio <silvio.traversaro@iit.it>
2025-02-24 09:54:33 +0100

    Add workarounds for finding Python on Windows on ROS 2 projects

--- cmake/Buildxcub-moveit2.cmake
+++ cmake/Buildxcub-moveit2.cmake
@@ -7,6 +7,14 @@ include(FindOrBuildPackage)
 find_or_build_package(YARP QUIET)
 find_or_build_package(yarp-devices-ros2 QUIET)
 
+# Workaround for ROS 2 Humble Python find cmake logic on Windows,
+# drop when we do not support ROS 2 Humble anymore
+# See https://github.com/robotology/robotology-superbuild/pull/1800#issuecomment-2677023043
+set(LOCAL_CMAKE_ARGS "")
+if(MSVC AND NOT ROBOTOLOGY_GENERATE_CONDA_RECIPES)
+  find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+  set(LOCAL_CMAKE_ARGS "-DPYTHON_EXECUTABLE=${Python3_EXECUTABLE} -DPython_EXECUTABLE=${Python3_EXECUTABLE} -DPython3_EXECUTABLE=${Python3_EXECUTABLE}")
+endif()
 
 ycm_ep_helper(xcub-moveit2 TYPE GIT
                                 STYLE GITHUB
@@ -14,6 +22,7 @@ ycm_ep_helper(xcub-moveit2 TYPE GIT
                                 TAG master
                                 COMPONENT core
                                 FOLDER src
+                                CMAKE_ARGS ${LOCAL_CMAKE_ARGS}
                                 DEPENDS YARP yarp-devices-ros2
                                 # This is because xcub-moveit2 is a ros-style repository
                                 # composed by multiple CMake projects, for compatibility with CMake
--- cmake/Buildyarp-devices-ros2.cmake
+++ cmake/Buildyarp-devices-ros2.cmake
@@ -7,10 +7,20 @@ include(FindOrBuildPackage)
 find_or_build_package(YCM QUIET)
 find_or_build_package(YARP QUIET)
 
+# Workaround for ROS 2 Humble Python find cmake logic on Windows,
+# drop when we do not support ROS 2 Humble anymore
+# See https://github.com/robotology/robotology-superbuild/pull/1800#issuecomment-2677023043
+set(LOCAL_CMAKE_ARGS "")
+if(MSVC AND NOT ROBOTOLOGY_GENERATE_CONDA_RECIPES)
+  find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+  set(LOCAL_CMAKE_ARGS "-DPYTHON_EXECUTABLE=${Python3_EXECUTABLE} -DPython_EXECUTABLE=${Python3_EXECUTABLE} -DPython3_EXECUTABLE=${Python3_EXECUTABLE}")
+endif()
+
 ycm_ep_helper(yarp-devices-ros2 TYPE GIT
                                      STYLE GITHUB
                                      REPOSITORY robotology/yarp-devices-ros2.git
                                      TAG master
                                      COMPONENT core
                                      FOLDER src
+                                     CMAKE_ARGS ${CMAKE_ARGS}
                                      DEPENDS YCM YARP)
