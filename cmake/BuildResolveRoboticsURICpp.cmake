2f8fe0f9cef0efa8c9e5df98d5685e7c91c303f4
Silvio Traversaro <silvio.traversaro@iit.it>
2025-03-08 15:56:22 +0100

    Use robotology-superbuild's YCM in ResolveRoboticsURICpp and bump version to 0.0.3 (#1819)
    * Use robotology-superbuild's YCM in ResolveRoboticsURICpp
    
    * Bump ResolveRoboticsURICpp to 0.0.3
    
    * Update BuildResolveRoboticsURICpp.cmake

--- cmake/BuildResolveRoboticsURICpp.cmake
+++ cmake/BuildResolveRoboticsURICpp.cmake
@@ -4,12 +4,16 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
+find_or_build_package(YCM QUIET)
+
 ycm_ep_helper(ResolveRoboticsURICpp TYPE GIT
                                     STYLE GITHUB
                                     REPOSITORY ami-iit/resolve-robotics-uri-cpp.git
                                     TAG main
                                     COMPONENT core
-                                    FOLDER src)
+                                    FOLDER src
+                                    CMAKE_ARGS -DBUILD_TESTING:BOOL=OFF -DRRUP_USE_SYSTEM_YCM:BOOL=ON
+                                    DEPENDS YCM)
 
 set(ResolveRoboticsURICpp_CONDA_PKG_NAME libresolve-robotics-uri-cpp)
 set(ResolveRoboticsURICpp_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
