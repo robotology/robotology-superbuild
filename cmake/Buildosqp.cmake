da7c57770b42dde3686a503bbd50362ddf27d369
Silvio Traversaro <silvio.traversaro@iit.it>
2025-06-04 15:16:39 +0200

    Update Buildosqp.cmake

--- cmake/Buildosqp.cmake
+++ cmake/Buildosqp.cmake
@@ -16,3 +16,8 @@ ycm_ep_helper(osqp TYPE GIT
 
 set(osqp_CONDA_PKG_NAME libosqp)
 set(osqp_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
+# This is a small hack. To avoid incompatibilities between the version tagged in the robotology-dependencies fork
+# (something like 0.6.3.x) and the version available in conda-forge when generating conda metapackages
+# such as robotology-distro, we override the conda package version of manif
+# here. This needs to be removed as soon as we stop using our fork in the superbuild 
+set(osqp_CONDA_VERSION 0.6.3)
