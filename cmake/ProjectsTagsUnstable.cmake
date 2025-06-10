52384c8a6858a242b91407b17974ec942cb7b198
Silvio Traversaro <silvio.traversaro@iit.it>
2025-06-10 23:19:59 +0200

    Upgrade osqp to v1.0.0 in LatestReleases and Stable, Unstable branches (#1863)

--- cmake/ProjectsTagsUnstable.cmake
+++ cmake/ProjectsTagsUnstable.cmake
@@ -6,11 +6,13 @@ endmacro()
 
 # External projects
 set_tag(osqp_REPOSITORY robotology-dependencies/osqp.git)
-set_tag(osqp_TAG v0.6.3.1)
+set_tag(osqp_TAG v1.0.0.1)
+set_tag(osqp-matlab_TAG v0.9.0.1)
 set_tag(manif_TAG 0.0.5)
 set_tag(CppAD_TAG 20250000.2)
 set_tag(proxsuite_TAG v0.7.1)
-set_tag(casadi_TAG 3.7.0)
+set_tag(casadi_REPOSITORY ami-iit/casadi.git)
+set_tag(casadi_TAG 3.7.0.1)
 set_tag(casadi-matlab-bindings_TAG v3.7.0.0)
 
 # Robotology projects
