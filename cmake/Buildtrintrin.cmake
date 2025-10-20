4209f6b89455b4251c0529e1fd4566e6e960e0ad
Silvio Traversaro <silvio@traversaro.it>
2025-10-20 13:40:37 +0200

    Add trintrin package

---
+++ cmake/Buildtrintrin.cmake
@@ -0,0 +1,25 @@
+# Copyright (C) 2018  iCub Facility, Istituto Italiano di Tecnologia
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
+
+if(ROBOTOLOGY_USES_PYTHON) 
+  # If we are not generating a conda recipe, the bindings need to go in the ROBOTOLOGY_SUPERBUILD_PYTHON_INSTALL_DIR
+  list(APPEND TRINTRIN_OPTIONAL_CMAKE_ARGS "-DTRINTRIN_PYTHON_INSTALL_DIR=${ROBOTOLOGY_SUPERBUILD_PYTHON_INSTALL_DIR}")
+endif()
+
+
+ycm_ep_helper(trintrin TYPE GIT
+              STYLE GITHUB
+              REPOSITORY ami-iit/trintrin.git
+              TAG master
+              COMPONENT dynamics
+              FOLDER src
+              CMAKE_ARGS-DTRINTRIN_COMPILE_PYTHON_BINDINGS:BOOL=${ROBOTOLOGY_USES_PYTHON} ${TRINTRIN_OPTIONAL_CMAKE_ARGS}
+              DEPENDS YARP)
+
+set(TRINTRIN_CONDA_PKG_NAME trintrin)
+set(TRINTRIN_CONDA_PKG_CONDA_FORGE_OVERRIDE ON)
